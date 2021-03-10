e7faab7f33cc4ac3d7021cb3891108d2b74783bc
Flow191 <Florianxschmidt@web.de>
2021-03-10 15:40:18 +0100

    Create Klassifizierung.R

---
+++ Klassifizierung.R
@@ -0,0 +1,10 @@
+library(classyfireR)
+
+# Einlesen der txt-Datei mit den InChiKeys
+data <- read_table("Data/InChiKeys.txt")
+
+# Keys als Vektor ausgeben
+InChI_Keys <- pull(data)
+
+# InChIKeys benutzen um zu klassifizieren
+Classification_List <- purrr::map(InChI_Keys, get_classification)
