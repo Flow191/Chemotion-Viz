da2f6a3266d969aeefe56294e2f52c4f294eabc3
Flow191 <Florianxschmidt@web.de>
2021-03-15 13:35:04 +0100

    Update Klassifizierung.R

--- Klassifizierung.R
+++ Klassifizierung.R
@@ -9,3 +9,8 @@ InChI_Keys <- pull(data)
 
 # InChIKeys benutzen um zu klassifizieren
 Classification_List <- purrr::map(InChI_Keys, get_classification)
+
+#Zugriff auf die class der 1. Substanz
+Classification_List[[1]]@classification[["Classification"]][3]
+
+
