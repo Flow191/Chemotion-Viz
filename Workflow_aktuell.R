81cafecf3f826db7ae50554109ccc75bdb4e3f45
Flow191 <Florianxschmidt@web.de>
2021-03-16 11:43:53 +0100

    Update Workflow_aktuell.R

--- Workflow_aktuell.R
+++ Workflow_aktuell.R
@@ -1,41 +1,40 @@
+library(readr)
+library(rinchi)
 library(magrittr)
 library(classyfireR)
-library(readr)
 library(dplyr)
 
-# Einlesen der SMILES aus CSV-Datei
-smiles <- read.csv("Data/Samples_SMILES.csv")
+# Die erhaltene Exceldatei aus Chemotion Repository muss vor dem Start dieses
+# Skriptes leicht angepasst werden. Dazu zählt: 
+# 1. Entfernen der Lösungsmittel (solvents),der Reaktionspartner (reactants) 
+# und der SMILES Duplikate
+# 2. Speichern der XLSX-Datei als CSV-Datei
 
-# Smiles als Vektor ausgeben
-smiles_vec <- pull(smiles)
+##############################################################################
 
-# Funktion zur Umwandlung von Smiles zu InChiKey
-smiles2inchikeys <- function(smiles) {
-  if (length(smiles)<1) {
-    warning("Empty smiles list")
-    return (character(0))
-  }
-  
-  inchikeys <- sapply(smiles, function(smile) {
-    filename=tempfile()
-    cat(smile, file=filename)
-    
-    con <- pipe(paste('obabel -i smi ',filename ,' -o inchi -xK  -O /dev/stdout 2>/dev/null | tr -d "\\t" ', sep=""))
-    
-    inchikey <- readLines(con)
-    close(con)
-    
-    system(paste("rm ", filename))
-    ifelse(length(inchikey)>0, inchikey, "") 
-  })
-  inchikeys
-}
+# Einlesen der SMILES-Spalte aus CSV-Datei
+smiles <- read.csv("Data/sample_export_16.03.2021_8.12_noDup.csv")[ ,5]
 
-# Funktionsausgabe als Vektor
-inchikey_vec <- smiles2inchikeys(smiles_vec)
+# Umwandlung von Smiles zu InChiKey
+# https://rdrr.io/github/CDK-R/rinchi/man/getinchi.html#heading-0
+inchikey <- sapply(smiles, get.inchi.key)
 
 # InChIKeys benutzen um zu klassifizieren
-Classification_List <- purrr::map(inchikey_vec, get_classification)
+Classification_List <- purrr::map(inchikey, get_classification)
 
-#Zugriff auf die class der 1. Substanz
-Classification_List[[8]]@classification[["Classification"]][3]
+# Zugriff auf die class der 1. Substanz
+Classification_List[[1]]@classification[["Classification"]][3]
+
+# Schleife in der die class der ersten 10 Substanzen angezeigt wird
+# NULL = leeres Objekt (zum Beispiel der "Datentyp" einer Funktion ohne Rückgabewert)
+# Fehler: trying to get slot "classification" from an object of a basic class 
+# ("NULL") with no slots
+for (i in 15:16) {
+  print(Classification_List[[i]]@classification[["Classification"]][3])
+}
+
+# Schleife in der das Objekt der ersten 10 Substanzen angezeigt wird
+for (i in 1:10) {
+  print(Classification_List[[i]])
+}
+
