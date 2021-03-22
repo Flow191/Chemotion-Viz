e036c364389203acdee6efdf2ba0dc856ee66855
Flow191 <Florianxschmidt@web.de>
2021-03-22 14:36:06 +0100

    Update Workflow_aktuell.R

--- Workflow_aktuell.R
+++ Workflow_aktuell.R
@@ -3,6 +3,9 @@ library(rinchi)
 library(magrittr)
 library(classyfireR)
 library(dplyr)
+library(sunburstR)
+library(Rcpp)
+library(RMassBank)
 
 # Die erhaltene Exceldatei aus Chemotion Repository muss vor dem Start dieses
 # Skriptes leicht angepasst werden. Dazu zählt: 
@@ -27,11 +30,15 @@ Classification_List <- purrr::map(inchikey, get_classification)
 level=3
 
 # Schleife in der die class aller Substanzen angezeigt wird
-class <- vector("character", 2343)
+class <- vector("character", length(Classification_List[]))
 for (i in 1:length(Classification_List[])) {
   if( is.null(Classification_List[[i]]) == 'TRUE') { 
-  } else { 
-    class[i] <- Classification_List[[i]]@classification[["Classification"]][level]}
+  } 
+  else if (is.null(Classification_List[[i]]@classification[["Classification"]][level]) == 'TRUE'){
+  }
+  else { 
+    class[i] <- Classification_List[[i]]@classification[["Classification"]][level]
+  }
 }
 
 # Leere Strings als NA markieren und entfernen
@@ -41,4 +48,13 @@ class[!is.na(class)]
 # Sortieren und zählen der classes
 class_sorted <- sort(class)
 df <- data.frame(class_sorted)
-count(df,class_sorted)
+n_classes <- count(df,class_sorted)
+
+# Ein erster Sunburst Plot
+sunburst(data = data.frame(n_classes), legend = FALSE)
+
+# Smiles to Mass
+mass_func <- sapply(smiles, smiles2mass)
+df_mass <- data.frame(as.list(mass_func))
+substance_mass <- t(df_mass)
+mass <- data.frame(substance= row.names(substance_mass), substance_mass, row.names=NULL) 
