8491e0197bc91750516eb056cbe9fd46e1144790
Flow191 <Florianxschmidt@web.de>
2021-03-23 16:02:11 +0100

    Update Knitr_Workflow.R

--- Knitr_Workflow.R
+++ Knitr_Workflow.R
@@ -5,15 +5,19 @@ library(magrittr)
 library(classyfireR)
 library(dplyr)
 library(sunburstR)
+library(Rcpp)
+library(RMassBank)
 
 # ---- smiles ----
 smiles <- read.csv("~/R/Chemotion/Chemotion/Data/sample_export_16.03.2021_8.12_noDup.csv")[ ,5]
+head(smiles,3)
 
 # ---- smiles_to_InChiKey ---- 
 inchikey <-sapply(smiles, get.inchi.key)
 
 # ---- classification ----
 Classification_List <- purrr::map(inchikey, get_classification)
+head(Classification_List,3)
 
 # ---- level ----
 level=3
@@ -29,6 +33,8 @@ for (i in 1:length(Classification_List[])) {
     class[i] <- Classification_List[[i]]@classification[["Classification"]][level]
   }
 }
+
+# ---- n_substances ----
 i
 
 # ---- null ----
@@ -39,7 +45,16 @@ class[!is.na(class)]
 class_sorted <- sort(class)
 df <- data.frame(class_sorted)
 (n_classes <- count(df,class_sorted))
+
+# ---- n_substances_class ----
 sum(n_classes$n)
 
 # ---- plot ----
 sunburst(data = data.frame(n_classes), legend = FALSE)
+
+# ---- smiles2mass ----
+mass_func <- sapply(smiles, smiles2mass)
+df_mass <- data.frame(as.list(mass_func))
+substance_mass <- t(df_mass)
+mass <- data.frame(substance= row.names(substance_mass), substance_mass, row.names=NULL)
+head(mass,3)
