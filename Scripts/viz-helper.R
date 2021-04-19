b2143d1e7d0ce6535934a70c171427236b38ab70
Flow191 <79848862+Flow191@users.noreply.github.com>
2021-04-19 14:39:24 +0200

    Helper-functions

---
+++ Scripts/viz-helper.R
@@ -0,0 +1,21 @@
+smiles2mass <- function(SMILES){
+  massfromformula <- parse.smiles(SMILES)[[1]]
+  do.typing(massfromformula)
+  do.aromaticity(massfromformula)
+  convert.implicit.to.explicit(massfromformula)
+  do.isotopes(massfromformula)
+  mass <- get.exact.mass(massfromformula)
+  return(mass)}
+
+classyfire2df <- function() {
+for (i in 1:length(Classification_List[])) {
+  if( is.null(Classification_List[[i]]) == 'TRUE') { 
+  } 
+  else if (is.null(Classification_List[[i]]@classification[["Classification"]][1]) == 'TRUE'){
+  }
+  else { 
+    class[i] <-as.data.frame(Classification_List[[i]]@classification[["Classification"]])
+  }
+}
+df <- plyr::ldply(class, rbind)
+return(df)}
