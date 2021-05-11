c472410551c8c0caf10c602e43b7d7f3914aaefb
Flow191 <79848862+Flow191@users.noreply.github.com>
2021-05-11 11:38:56 +0200

    Update

--- Scripts/viz-helper.R
+++ Scripts/viz-helper.R
@@ -63,3 +63,40 @@ bar_func <- function() {
   df <-ddply(df,.(classes),summarize, size=length(classes) )
   return(df)
 }
+
+starting_mats <- function(value) {
+  inds <- which(value == lead(value))  
+  sort(unique(c(inds, inds + 1, inds +2)))
+}
+
+classyfire2df_e <- function() {
+  class <- vector("character", length(Classification_List_start_mats[]))
+  for (i in 1:length(Classification_List_start_mats[])) {
+    if( is.null(Classification_List_start_mats[[i]]) == 'TRUE') { 
+    } 
+    else if (is.null(Classification_List_start_mats[[i]]@classification[["Classification"]][1]) == 'TRUE'){
+    }
+    else { 
+      class[i] <-as.data.frame(Classification_List_start_mats[[i]]@classification[["Classification"]])
+    }
+  }
+  char_list <-lapply(class, as.character)
+  df_start_mats <- plyr::ldply(char_list, rbind)
+  return(df_start_mats)
+}
+
+classyfire2df_p <- function() {
+  class2 <- vector("character", length(Classification_List_start_mats[]))
+  for (i in 1:length(Classification_List_products[])) {
+    if( is.null(Classification_List_products[[i]]) == 'TRUE') { 
+    } 
+    else if (is.null(Classification_List_products[[i]]@classification[["Classification"]][1]) == 'TRUE'){
+    }
+    else { 
+      class2[i] <-as.data.frame(Classification_List_products[[i]]@classification[["Classification"]])
+    }
+  }
+  char_list2 <-lapply(class2, as.character)
+  df_products <- plyr::ldply(char_list2, rbind)
+  return(df_products)
+}
